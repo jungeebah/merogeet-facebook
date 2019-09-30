ff37eafa74852f57660a2a4bebb67415f802af0e
jungee <jungeebah@gmail.com>
2019-09-29 22:15:00 -0700

    changed the about call

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const port = process.env.PORT || 8080;
 
 
 var clean_url = function(url){
-    myURL = new URL(url)
-    var pos1 = myURL.pathname.indexOf("\/"); 
-    var pos2 = myURL.pathname.indexOf('\/',pos1+1);
-    url = myURL.origin+myURL.pathname.slice(0,pos2);
     url = url.replace(/\/+$/, "");
     return url;
 }
@@ -41,56 +37,60 @@ function facebook_search(facebook_url){
     return new Promise(async (resolve, reject) => {
         try{
             var cleaned_url = clean_url(facebook_url);
-            var url = cleaned_url + '/about/'
-                const browser = await puppeteer.launch({
-                    args: ['--no-sandbox', '--disable-setuid-sandbox']
-                });
-                const page = await browser.newPage();
-                page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36')
+            if (cleaned_url.indexOf('about')>0){
+                var url = cleaned_url
+            }else{
+                var url = cleaned_url + '/about/'
+            }
+            
+            const browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+            const page = await browser.newPage();
+            page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36')
 
-                await page.goto(url);
-                await page.waitForSelector(".img");
+            await page.goto(url);
+            await page.waitForSelector(".img");
 
-                try{
-                    const infos = await page.$$('div[class="_5aj7 _3-8j"]');
-                    for (const info of infos){
-                        const photo_link = await info.$eval('img', img => img.src);
-                        const inner_text = await page.evaluate(info => info.innerText,info); 
-                        
-                        var web_url=null;
-                        if (sanitize_url(inner_text)){
-                             web_url = sanitize_url(inner_text);
-                        }
-                        if (photo_link.indexOf('EaDvTjOwxIV') >0 || web_url){
-                            jsonData["website"] = inner_text;
-                        }
-                        else if(photo_link.indexOf('8TRfTVHth97') > 0 || inner_text.indexOf('instagram')>0){
-                            jsonData["instagram"] = inner_text;
-                        }
-                        else if (photo_link.indexOf('WPSrBl8J06p') >0|| inner_text.indexOf('twitter')>0){
-                            jsonData["twitter"]=inner_text;
-                        }
-                        else if (photo_link.indexOf('RTHgMeQuiIN') >0 || photo_link.indexOf('7xu6qkZsbtP') >0 || inner_text.indexOf('youtube')>0){
-                            if (match_regex(inner_text)){
-                                jsonData["youtube"]=inner_text;
-                            }else{
-                                continue;
-                            }
-                        }
-                        else if (photo_link.indexOf('PPpFwUhDmz') >0|| inner_text.indexOf('soundlcoud')>0){
-                            jsonData["soundcloud]"]= inner_text;
+            try{
+                const infos = await page.$$('div[class="_5aj7 _3-8j"]');
+                for (const info of infos){
+                    const photo_link = await info.$eval('img', img => img.src);
+                    const inner_text = await page.evaluate(info => info.innerText,info); 
+                    var web_url=null;
+                    if (sanitize_url(inner_text)){
+                            web_url = sanitize_url(inner_text);
+                    }
+                    if (photo_link.indexOf('EaDvTjOwxIV') >0 || web_url){
+                        jsonData["website"] = inner_text;
+                    }
+                    else if(photo_link.indexOf('8TRfTVHth97') > 0 || inner_text.indexOf('instagram')>0){
+                        jsonData["instagram"] = inner_text;
+                    }
+                    else if (photo_link.indexOf('WPSrBl8J06p') >0|| inner_text.indexOf('twitter')>0){
+                        jsonData["twitter"]=inner_text;
+                    }
+                    else if (photo_link.indexOf('RTHgMeQuiIN') >0 || photo_link.indexOf('7xu6qkZsbtP') >0 || inner_text.indexOf('youtube')>0){
+                        if (match_regex(inner_text)){
+                            jsonData["youtube"]=inner_text;
+                        }else{
+                            continue;
                         }
                     }
-                    await browser.close();
-                    return resolve(jsonData);
+                    else if (photo_link.indexOf('PPpFwUhDmz') >0|| inner_text.indexOf('soundlcoud')>0){
+                        jsonData["soundcloud]"]= inner_text;
+                    }
                 }
-                catch(e){
-                    return reject(e)
-                }
+                await browser.close();
+                return resolve(jsonData);
             }
             catch(e){
                 return reject(e)
             }
+        }
+        catch(e){
+            return reject(e)
+        }
     })
 }
 
